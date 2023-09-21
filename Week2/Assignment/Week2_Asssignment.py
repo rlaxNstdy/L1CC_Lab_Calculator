b27b4afbffd43c47dca81bcbe2fbbfe247515498
Francesco Emmanuel <114067626+FrancescoEmmanuel@users.noreply.github.com>
2023-09-21 17:22:27 +0700

    Add files via upload

---
+++ Week2/Assignment/Week2_Asssignment.py
@@ -0,0 +1,12 @@
+'''
+Assignment:
+
+- Create a math calculator program
+- It should:
+  1. be able to calculate non whole number digits.
+  2. takes in user input for the numbers.
+  3. is able to calculate using all basic arithmetic operations(addition, subtraction, multiplication, division, power) - you can also go the extra mile by including roots.
+  4. does not produce an error when user does not input a number.
+
+- (optional): Create another type of calculator inside the program (example: BMI calculator, financial calculators, etc)
+'''
